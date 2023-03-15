5556d87cece336219af2285bdf35d189f9d8120d
yehorpapkovych <127139325+yehorpapkovych@users.noreply.github.com>
2023-03-15 15:17:25 +0200

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -2,7 +2,6 @@ name = input('Enter your name: ')
 age = input('Enter your age: ')
 if age.isdigit():
     age1 = int(age) + 1
-    str(age1)
     print(f"Hello {name}, on your next birthday you'll be {age1} years")
 else:
     print('Write your AGE, please!')
